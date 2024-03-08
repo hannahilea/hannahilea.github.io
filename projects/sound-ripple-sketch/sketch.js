e8fe4113c621a5f878a02129c33f7a112e593e24
hannahilea <hannahilea@users.noreply.github.com>
2024-03-08 13:46:44 -0500

    lint

--- projects/sound-ripple-sketch/sketch.js
+++ projects/sound-ripple-sketch/sketch.js
@@ -19,40 +19,40 @@ function setup() {
   mic.start();
 }
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function spawnParticles(volume) {
   let numParticles = volume * 100;
   for (let i = 0; i < numParticles; i++) {
-    
+
     // Different particle modes:
     // Square:
     // let particle = {x: mouseX, y: mouseY, energy: volume, dX: random(-1.0,1.0), dY: random(-1.0,1.0), life: 1.0};
-    
+
     // Funky 1d diagonal lines
     // let d = random(-1.0,1.0);
     // let particle = {x: mouseX, y: mouseY, energy: volume, dX: d, dY: d, life: 1.0};
-    
+
     // Funky x's
     // let d = random(-1.0,1.0);
     // let particle = {x: mouseX, y: mouseY, energy: volume, dX: random([-1,1]) * d, dY: random([-1,1]) * d, life: 1.0};
-    
+
     // Circular rings! Like water drops
-    let d = random(0, 2*PI);
-    let particle = {x: mouseX, y: mouseY, energy: volume, dX: random([-1,1]) * cos(d), dY: random([-1,1]) * sin(d), life: volume * 8};
-    
+    let d = random(0, 2 * PI);
+    let particle = { x: mouseX, y: mouseY, energy: volume, dX: random([-1, 1]) * cos(d), dY: random([-1, 1]) * sin(d), life: volume * 8 };
+
     // let d = random(0, 2*PI);
     // let particle = {x: mouseX, y: mouseY, energy: volume, dX: random(0,1) * cos(d), dY: random(0,1) * sin(d), life: volume * 8};
-    
+
     livingParticles.push(particle);
   }
 }
 
 function draw() {
   background("darkblue");
-  
+
   // https://stackoverflow.com/questions/55026293/google-chrome-javascript-issue-in-getting-user-audio-the-audiocontext-was-not
   getAudioContext().resume();
 
@@ -61,19 +61,19 @@ function draw() {
   if (vol > 0.04) {
     spawnParticles(vol);
   }
-  
+
   if (true) {
-    livingParticles = livingParticles.filter( (particle) => {
+    livingParticles = livingParticles.filter((particle) => {
       return particle.life > 0;
     });
   }
-    
-  livingParticles.forEach( (particle) => {   
+
+  livingParticles.forEach((particle) => {
     fill(particle.energy, 0.75, 0.5);
     noStroke();
-      ellipse(particle.x + particle.dX, particle.y + particle.dY, particle.life * 5, particle.life * 5);
-      particle.x += particle.dX;
-      particle.y += particle.dY;
-      particle.life -= .005;
+    ellipse(particle.x + particle.dX, particle.y + particle.dY, particle.life * 5, particle.life * 5);
+    particle.x += particle.dX;
+    particle.y += particle.dY;
+    particle.life -= .005;
   });
 }
