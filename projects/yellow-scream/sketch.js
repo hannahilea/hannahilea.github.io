3b4e94d64f186bb825781b6e4ac1d7737d388a32
hannahilea <hannahilea@users.noreply.github.com>
2024-03-28 13:27:24 -0400

    wip formatting

--- projects/yellow-scream/sketch.js
+++ projects/yellow-scream/sketch.js
@@ -8,16 +8,33 @@ let mic;
 let x;
 let y;
 let yellowColor;
-let brushHeight;
-let isPainting;
+let isPainting = false;
+let currentBrushHeight = 30;
+
+const params = {
+  brushHeight: 30,
+  margin: 30,
+  yellSensitivity: 0.02,
+  brushYWiggle: 0.2,
+};
+
+const gui = new GUI();
+gui.add(params, 'brushHeight', 1, 100, 5);
+gui.add(params, 'margin', 0, 100, 1);
+gui.add(params, 'yellSensitivity', 0.01, 10, .1);
+gui.add(params, 'brushYWiggle', 0, .6, .1);
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth - 2 * params.margin, windowHeight - 2 * params.margin);
   colorMode(RGB, 255);
-  background("white");
-  isPainting = false;
+
+  // Set up painting canvas
+  background("#515763");
+  fill("#E8E7D7");
+  rect(params.margin, params.margin, windowWidth - 2 * params.margin, windowHeight - 2 * params.margin);
+
+  // ...and pallette
   yellowColor = color(255, 204, 0);
-  brushHeight = 30;
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -32,18 +49,20 @@ function draw() {
   getAudioContext().resume();
 
   // Get the overall volume (between 0 and 1.0)
-  let vol = mic.getLevel();
-  let yellThreshold = 0.02;
-  if (vol > yellThreshold && !isPainting) {
+  let vol = mic.getLevel() * 10;
+  // console.log(vol);
+  if (vol > params.yellSensitivity && !isPainting) {
     // Start a stroke
-    x = random(width);
-    y = random(height);
+    x = random(width - 2 * params.margin) + params.margin;
+    y = random(height - 2 * params.margin) + params.margin;
     yellowColor = color(255, 204 + random(-10, 100), 0);
     isPainting = true;
-    brushHeight = 30 + random(-1.0, 1.0);
-  } else if (vol > yellThreshold && isPainting) {
+    currentBrushHeight = params.brushHeight + random(-1.0, 1.0);
+  } else if (vol > params.yellSensitivity && isPainting) {
     // Continuing a stroke
-    x -= 0.5;
+
+    if (x <= params.margin + 0.5) { x = params.margin + 0.5; } else { x -= 0.5; }
+    y += random(-params.brushYWiggle, params.brushYWiggle);
   } else if (isPainting) {
     // no noise is happening, stop painting
     isPainting = false;
@@ -52,6 +71,6 @@ function draw() {
   if (isPainting) {
     fill(yellowColor);
     noStroke();
-    square(x, y, brushHeight, 0);
+    square(x, y, currentBrushHeight, 0);
   }
 }
