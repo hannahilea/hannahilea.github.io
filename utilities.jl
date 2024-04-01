a64e0f0078e0571846a53c09921d84d1aaec1e14
hannahilea <hannahilea@users.noreply.github.com>
2024-04-01 13:45:10 -0400

    add templated project adding to project index

--- utilities.jl
+++ utilities.jl
@@ -1,6 +1,10 @@
+const NEW_PROJ_COMMENT = "<!-- Add new project here -->"
+
 # Some quick and brittle Julia utilities for adding new projects
 # Doesn't handle nested, only does new js projects
 # Will fail if dir already exists
+# TODO: function to add different types of projects
+# TODO: validate git state isn't dirty before starting
 function new_js_project()
     project_name = lstrip(rstrip(Base.prompt("Enter project name: ")))
     dir_name = lstrip(rstrip(Base.prompt("Enter project url name: ")))
@@ -16,6 +20,15 @@ function new_js_project()
         write(file, str)
     end
 
-    # TODO: update projects index!!
-
+    @info "Adding new project to project index"
+    let
+        projects_file = "projects.html"
+        new_blob = """\n        <li><a href="./projects/$(dir_name)">\n            <h3>$(project_name)</h3>\n          </a>\n          <p>TODO-description</p>\n        </li>"""
+        str = read(projects_file, String)
+        i = findfirst(NEW_PROJ_COMMENT, file)
+        isnothing(i) && throw(ArgumentError("Oh no, $(NEW_PROJ_COMMENT) not found in projects.html!"))
+        str = str[1:last(i)] * new_blob * str[last(i)+1:end]
+        write(projects_file, str)
+    end
+    @info "Do ctrl+f TODO to find regions to update for newly added project!"
 end
