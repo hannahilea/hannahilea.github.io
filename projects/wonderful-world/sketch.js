ef7224d6dcaf9a30f2a204774aa0f49f54865268
hannahilea <hannahilea@users.noreply.github.com>
2024-04-01 22:07:48 -0400

    tweak params

--- projects/wonderful-world/sketch.js
+++ projects/wonderful-world/sketch.js
@@ -3,57 +3,27 @@
 // Recurse Center (www.recurse.com/)
 // https://editor.p5js.org/hannahilea/sketches/AWA1W-c3H
 
+let angle = 0;
+let earthSize = 200;
+let earthImg;
 
-// let mic;
+let mySound;
+function preload() {
+  soundFormats('mp3');
+  mySound = loadSound('world.mp3');
+  earthImg = loadImage('earth3.jpeg');
+}
 
-const params = {
-  // micSensitivity: 4.0,
-};
-
-const gui = new GUI();
-// gui.add(params, 'micSensitivity', 0.01, 30, 2);
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight)
-
-//   // Create an Audio input
-//   mic = new p5.AudioIn();
-
-//   // start the Audio Input.
-//   // By default, it does not .connect() (to the computer speakers)
-//   mic.start();
-// }
+function setup() {
+  let c = createCanvas(windowWidth, windowHeight, WEBGL);
+  c.mousePressed(playPauseSong);
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// function draw() {
-//   // https://stackoverflow.com/questions/55026293/google-chrome-javascript-issue-in-getting-user-audio-the-audiocontext-was-not
-//   getAudioContext().resume();
-// }
-
-
-let angle = 0;
-let r = 200;
-let earth;
-
-let mySound;
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('world.mp3');
-  earth = loadImage('earth.jpg');
-}
-
-function setup() {
-  let cnv = createCanvas(500, 500, WEBGL);
-  cnv.mousePressed(canvasPressed);
-  textAlign(CENTER, CENTER);
-}
-
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
+function playPauseSong() {
   if (mySound.isPlaying()) {
     mySound.pause();
   } else {
@@ -65,29 +35,37 @@ function draw() {
   // Get our values!
   rate = map(mouseY, 0, height, 0.5, 2);
   let spinAmt;
-  if (mouseY < height/2) {
+  if (!mySound.isPlaying()) {
+    spinAmt = 0;
+  } else if (mouseY < height / 2) {
     spinAmt = map(mouseY, 0, height, 0.001, 0.03, true)
     // console.log(spinAmt);
   } else {
-    // spinAmt = map(mouseY, 0, height, 0.02, 0.54, true)
-    spinAmt = map(mouseY-height/2, 0, height, 0.02, 0.5);
+    spinAmt = map(mouseY - height / 2, 0, height, 0.02, 0.5);
   }
   pan = map(mouseX, 0, width, -1., 1.);
-  
-  let c = color(map(pan, -1, 1, 0, 256), 
-                map(pan, -1, 1, 256, 0), 
-                0);
-  
+
+  // Draw background gradient
+  c1 = color(300);
+  let c2 = color(map(pan, -1, 1, 212, 251),
+    map(pan, -1, 1, 20, 176),
+    map(pan, -1, 1, 90, 51));
+  for (let y = -windowHeight / 2; y < windowHeight / 2; y++) {
+    n = map(y, -windowHeight / 2, windowHeight / 2, 0, 1);
+    let newc = lerpColor(c1, c2, n);
+    stroke(newc);
+    line(-windowWidth / 2, y, windowWidth / 2, y);
+  }
+
   // Draw stuff
-  background(c);
   rotateY(angle);
   lights();
   fill(200);
   noStroke();
   angle += spinAmt;
-  texture(earth);
-  sphere(r);
-  
+  texture(earthImg);
+  sphere(earthSize);
+
   // Update our audio
   mySound.rate(rate);
   mySound.pan(pan);
