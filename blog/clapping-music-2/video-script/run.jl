5b204aa59c90f285a5dfa7b01635c5e39770cd5f
hannahilea <hannahilea@users.noreply.github.com>
2024-09-27 16:35:38 -0400

    add scripts

---
+++ blog/clapping-music-2/video-script/run.jl
@@ -0,0 +1,145 @@
+# Generate content filmed for blog post
+# TODO add manifest
+
+using FlipBoard
+shared_srl = open_srl_iff_available(; portname="/dev/ttyS0", baudrate=57600)
+dots_sink = AZDotsSink(; address=0x00, serial_port=shared_srl)
+digits_sink = AZDigitsSink(; address=0x01, serial_port=shared_srl)
+both_boards_sink = all_alphazeta_sink(shared_srl)
+
+# Let's look at byte---i.e., a column of the board---and watch it count up in bits:
+for x in 0x00:0x7F
+    FlipBoard.all_dark(sink_dots)
+    sleep(0.1)
+    set_rand_discs(sink_dots; num_discs=1, available_byte_indices=x:x)
+    sleep(0.1)
+end
+
+# ...and now let's do the whole board, because it is cool (and so is the sweeping)
+for x in 0x00:0x7F
+    FlipBoard.all_dark(sink_dots)
+    sleep(0.2)
+    set_rand_discs(sink_dots; num_discs=28, available_byte_indices=x:x)
+    sleep(0.2)
+end
+
+# One cool thing I discovered is that if you unset a disc before it has had to 
+# properly flip, you'll still hear the very quiet solenoid noise. As more discs 
+# are triggered, there is some breakthrough pixel visibility
+for x in 0x00:0x7F
+    FlipBoard.all_dark(sink_dots)
+    sleep(0.2)
+    set_rand_discs(sink_dots; num_discs=10, available_byte_indices=x:x)
+end
+
+# ...does the speed at which the unset happens affect the volume?
+for s in 0.2:-0.01:0.0
+    FlipBoard.all_dark(sink_dots)
+    sleep(0.2)
+    set_rand_discs(sink_dots; num_discs=1, available_byte_indices=0x7F:0x7F)
+    sleep(s)
+end
+
+# ...and with no pauses at all! Single column, then block of columns: 
+for s in 0.3:-0.01:0.0
+    FlipBoard.all_dark(sink_dots)
+    set_rand_discs(sink_dots; num_discs=1, available_byte_indices=0x7F:0x7F)
+    sleep(s)
+end
+
+for s in 0.3:-0.01:0.0
+    FlipBoard.all_dark(sink_dots)
+    set_rand_discs(sink_dots; num_discs=10, available_byte_indices=0x7F:0x7F)
+    sleep(s)
+end
+
+# OKAY. Let's play some claping music.
+# clapping_music(; clap_a, clap_b, num_repeats=2)
+
+# 1. New full clapping music 
+FlipBoard.all_dark(sink_dots)
+board_state = zeros(UInt8, 28)
+num_cols = 10
+
+function clap_a!()
+    board_state[1:num_cols] = board_state[1:num_cols] .+ rand(0x00:0x7F, num_cols)
+    return write_to_sink(sink, board_state)
+end
+
+function clap_b!()
+    board_state[(end - num_cols + 1):end] = board_state[(end - num_cols + 1):end] .+ rand(0x00:0x7F, num_cols)
+    return write_to_sink(sink, board_state)
+end
+
+#...abridged:
+clapping_music(; clap_a=clap_a!, clap_b=clap_b!, num_repeats=2)
+
+# ...full:
+clapping_music(; clap_a, clap_b, num_repeats)
+
+# "THE LITTLEST CLAPPING HANDS" ...okay, didn't do what i expected BUT was okay!
+FlipBoard.all_dark(sink_dots)
+board_state = zeros(UInt8, 28)
+
+function clap_a!()
+    board_state[12:14] = mod1.(board_state[12:14] .+ 1, 256)
+    return write_to_sink(sink, board_state)
+end
+
+function clap_b!()
+    board_state[15:17] = mod1.(board_state[15:17] .+ 1, 256)
+    return write_to_sink(sink, board_state)
+end
+
+clapping_music(; clap_a=clap_a!, clap_b=clap_b!, num_repeats=2)
+
+
+# "THE LITTLEST CLAPPING HANDS" for real? Close...
+FlipBoard.all_dark(sink_dots)
+board_state = zeros(UInt8, 28)
+
+function clap_a!()
+    board_state[14] = mod1.(board_state[14] .+ 1, 2)
+    return write_to_sink(sink, board_state)
+end
+
+function clap_b!()
+    board_state[15] = mod1.(board_state[15] .+ 1, 2)
+    return write_to_sink(sink, board_state)
+end
+
+clapping_music(; clap_a=clap_a!, clap_b=clap_b!, num_repeats=2)
+
+# "THE LITTLEST CLAPPING HANDS" for really real? yes! (AF request)
+FlipBoard.all_dark(sink_dots)
+board_state = zeros(UInt8, 28)
+
+function clap_a!()
+    board_state[14] = board_state[14] == 0 ? 1 : 0
+    return write_to_sink(sink, board_state)
+end
+
+function clap_b!()
+    board_state[15] = board_state[15] == 0 ? 1 : 0
+    return write_to_sink(sink, board_state)
+end
+
+clapping_music(; clap_a=clap_a!, clap_b=clap_b!, num_repeats=2)
+
+# "THE BIGGEST CLAPPING HANDS" - Gets iffy after 3 rows (val=7), good with val=3 and 1
+FlipBoard.all_dark(sink_dots)
+board_state = zeros(UInt8, 28)
+
+function clap_a!()
+    val = board_state[14] == 0 ? 3 : 0
+    board_state[1:14] .= val
+    return write_to_sink(sink, board_state)
+end
+
+function clap_b!()
+    val = board_state[15] == 0 ? 3 : 0
+    board_state[15:end] .= val
+    return write_to_sink(sink, board_state)
+end
+
+clapping_music(; clap_a=clap_a!, clap_b=clap_b!, num_repeats=2, num_shifts=2)
---
+++ blog/clapping-music-for-flip-disc-displays/video-script/run.jl
@@ -0,0 +1,67 @@
+# Generate content filmed for blog post
+#TODO add manifest
+using FlipBoard
+
+shared_srl = open_srl_iff_available(; portname="/dev/ttyS0", baudrate=57600)
+dots_sink = AZDotsSink(; address=0x00, serial_port=shared_srl)
+digits_sink = AZDigitsSink(; address=0x01, serial_port=shared_srl)
+both_boards_sink = all_alphazeta_sink(shared_srl)
+
+# 1. Original/full clapping music 
+flash_reset(both_boards_sink)
+FlipBoard.clapping_music(dots_sink, digits_sink; pause=0.15, num_repeats=12,
+                         num_dots_to_set=28, num_digits_to_set=2)
+
+#1. Original - 2 repeats only, no prefix 
+flash_reset(both_boards_sink)
+FlipBoard._clapping_music(dots_sink, digits_sink; pause=0.15, num_repeats=2,
+                          num_dots_to_set=28, num_digits_to_set=2)
+sleep(1)
+flash_reset(both_boards_sink)
+
+# 2A. Demo individual segments: dots
+for c in ['.', ':', '!'], num_dots in 1:14
+    write_to_sink(dots_sink, text_to_bytes(dots_sink, repeat(c, num_dots)))
+    sleep(0.1)
+    all_dark(dots_sink)
+    sleep(0.1)
+end
+all_bright(dots_sink)
+sleep(0.1)
+all_dark(dots_sink)
+sleep(0.1)
+all_bright(dots_sink)
+sleep(0.1)
+all_dark(dots_sink)
+
+# 2B. Demo individual segments: dots
+for c in ['-', '1', '8'], num_digits in 1:28
+    write_to_sink(digits_sink, text_to_bytes(digits_sink, repeat(c, num_digits)))
+    sleep(0.1)
+    all_dark(digits_sink)
+    sleep(0.1)
+end
+all_bright(digits_sink)
+sleep(0.1)
+all_dark(digits_sink)
+sleep(0.1)
+all_bright(digits_sink)
+sleep(0.1)
+all_dark(digits_sink)
+
+# 3. Demo alternate approaches to clapping music
+# Pattern A
+all_dark(both_boards_sink)
+clap_pattern = Bool[1, 0, 0, 1, 1, 1]
+FlipBoard._clapping_music(dots_sink, digits_sink; clap_pattern, pause=0.12, num_repeats=2,
+                          num_dots_to_set=14, num_digits_to_set=4)
+sleep(1)
+all_dark(both_boards_sink)
+
+# Pattern B
+all_dark(both_boards_sink)
+clap_pattern = Bool[1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0]
+FlipBoard._clapping_music(dots_sink, digits_sink; clap_pattern, pause=0.15, num_repeats=2,
+                          num_dots_to_set=28, num_digits_to_set=14)
+sleep(1)
+all_dark(both_boards_sink)
