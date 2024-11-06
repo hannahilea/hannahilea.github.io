1d294479badf783a1bec164611c09b0da0e9ac02
hannahilea <hannahilea@users.noreply.github.com>
2024-11-06 17:28:34 -0500

    add save option

--- projects/yellow-scream/sketch.js
+++ projects/yellow-scream/sketch.js
@@ -61,7 +61,7 @@ function draw() {
   // console.log(vol);
   if (vol > params.screamThreshold && !isPainting) {
     // Start a stroke
-    x = random(width);
+    x = random(windowWidth);
     y = random(height);
     yellowColor = color(255, 204 + random(-10, 100), 0);
     isPainting = true;
@@ -81,4 +81,25 @@ function draw() {
     noStroke();
     square(x, y, currentbrushWidth, 0);
   }
+}
+
+function saveMasterpiece() {
+  let cnv = document.getElementById("p5-canvas");
+  let date = new Date().toISOString().split("T")[0];
+  let filename = 'YellowScream-' + date + ".png";
+  saveCanvas(cnv, filename, 'png' );
+}
+
+function getCanvasHeight() {
+  return 1 + windowHeight - document.getElementById("project-body").offsetTop;
+}
+
+function resetCanvas() {
+  let cnv = document.getElementById("p5-canvas");
+
+  let details = document.getElementsByClassName("projects-details")[0];
+  details.open = false;
+
+  resizeCanvas(windowWidth, getCanvasHeight(), cnv);
+  background("#E8E7D7");
 }
